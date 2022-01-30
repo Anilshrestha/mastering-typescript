40cc6c3b7be9347387097815306256bcaade344b
Anilshrestha <wedeerwalker@gmail.com>
2022-01-30 10:10:26 +0900

    inheritance example

--- ch03/inheritance.js
+++ ch03/inheritance.js
@@ -10,5 +10,70 @@ let multipleObject = {
     name: "myName",
     description: "myDescription"
 };
+console.log(multipleObject);
 // Class inheritance
+class BaseClass {
+    constructor() {
+        this.id = 0;
+    }
+}
+class DerivedFromBaseClass extends BaseClass {
+    constructor() {
+        super(...arguments);
+        this.name = "nameString";
+    }
+}
+class MultipleInterfaces {
+    constructor() {
+        this.id = 0;
+        this.name = "nameString";
+    }
+}
+// The super functioin
+class BaseClassWithCtor {
+    constructor(id) {
+        this.id = id;
+    }
+}
+class DeriveClassWithCtor extends BaseClassWithCtor {
+    constructor(id, name) {
+        super(id);
+        this.name = name;
+    }
+}
+class BaseClassWithFn {
+    print(text) {
+        console.log(`BaseClassWithFn.print() = ${text}`);
+    }
+}
+class DerivedClassWithFn extends BaseClassWithFn {
+    print(text) {
+        console.log(`DerivedClassWithFn.print() = ${text}`);
+    }
+}
+let deriveClassWithFn = new DerivedClassWithFn();
+deriveClassWithFn.print('anil');
+class DerivedClassFnCallThrough extends BaseClassWithFn {
+    print(text) {
+        super.print(`from DerivedClassFnCallThrough= ${text}`);
+    }
+}
+let derivedClassFnCallThrough = new DerivedClassFnCallThrough();
+derivedClassFnCallThrough.print('sunil');
+class BaseClassProtected {
+    constructor(id) {
+        this.name = "";
+        this.id = id;
+    }
+}
+class AccessProtected extends BaseClassProtected {
+    constructor(id) {
+        super(id);
+        console.log(`base.id= ${this.id}`);
+        // console.log(`base.name= ${this.name}`);
+    }
+}
+let accessProtected = new AccessProtected(1);
+// accessProtected.id = 1;
+// accessProtected.name = 'anil'; 
 //# sourceMappingURL=inheritance.js.map
--- ch03/inheritance.ts
+++ ch03/inheritance.ts
@@ -32,8 +32,99 @@ let multipleObject: IMultiple = {
   description: "myDescription"
 }
 
+console.log(multipleObject);
+
 
 // Class inheritance
+class BaseClass implements IBase {
+  id: number = 0;
+}
+
+class DerivedFromBaseClass 
+  extends BaseClass
+  implements IDerivedFromBase
+{
+  name: string = "nameString";
+} 
+
+
+interface IFirstInterface {
+  id: number;
+}
+interface ISecondInterface {
+  name: string;
+}
+
+class MultipleInterfaces implements 
+  IFirstInterface,
+  ISecondInterface
+{
+  id: number = 0;
+  name: string = "nameString";
+}
+
+// The super functioin
+
+class BaseClassWithCtor {
+  id: number
+  constructor(id: number) {
+    this.id = id;
+  }
+}
+
+class DeriveClassWithCtor extends BaseClassWithCtor {
+  private name: string
+  constructor(id: number, name: string) {
+    super(id);
+    this.name = name;
+  }
+}
+
+class BaseClassWithFn {
+  print(text: string) {
+    console.log(`BaseClassWithFn.print() = ${text}`);
+  }
+}
+
+class DerivedClassWithFn extends
+  BaseClassWithFn {
+    print(text: string) {
+      console.log(`DerivedClassWithFn.print() = ${text}`);
+    }
+  }
+
+  let deriveClassWithFn = new DerivedClassWithFn();
+  deriveClassWithFn.print('anil');
+
+class DerivedClassFnCallThrough extends
+  BaseClassWithFn {
+    print(text: string) {
+      super.print(`from DerivedClassFnCallThrough= ${text}`);
+    }
+  }
+
+let derivedClassFnCallThrough = new DerivedClassFnCallThrough();
+derivedClassFnCallThrough.print('sunil');
+
+class BaseClassProtected {
+  protected id: number;
+  private name: string = "";
+  constructor(id: number) {
+    this.id = id;
+  }
+}
+
+class AccessProtected extends BaseClassProtected {
+  constructor(id: number) {
+    super(id);
+    console.log(`base.id= ${this.id}`);
+    // console.log(`base.name= ${this.name}`);
+  }
+}
+
+let accessProtected = new AccessProtected(1);
+// accessProtected.id = 1;
+// accessProtected.name = 'anil'; 
 
 
 
